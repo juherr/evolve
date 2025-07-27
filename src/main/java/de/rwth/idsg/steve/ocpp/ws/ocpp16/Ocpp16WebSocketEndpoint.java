82fdb9f45edec00166e418c01ba6af1e5dbee94c
Julien Herr <jherr@bornerecharge.fr>
2025-07-27 12:59:09 +0200

    refactor: inject SteveConfiguration into WebSocket endpoints and SessionContextStoreImpl for improved configuration management

--- src/main/java/de/rwth/idsg/steve/ocpp/ws/ocpp16/Ocpp16WebSocketEndpoint.java
+++ src/main/java/de/rwth/idsg/steve/ocpp/ws/ocpp16/Ocpp16WebSocketEndpoint.java
@@ -20,6 +20,7 @@ package de.rwth.idsg.steve.ocpp.ws.ocpp16;
 
 import de.rwth.idsg.ocpp.jaxb.RequestType;
 import de.rwth.idsg.ocpp.jaxb.ResponseType;
+import de.rwth.idsg.steve.SteveConfiguration;
 import de.rwth.idsg.steve.config.DelegatingTaskScheduler;
 import de.rwth.idsg.steve.ocpp.OcppProtocol;
 import de.rwth.idsg.steve.ocpp.OcppVersion;
@@ -60,8 +61,9 @@ public class Ocpp16WebSocketEndpoint extends AbstractWebSocketEndpoint {
                                    OcppServerRepository ocppServerRepository,
                                    FutureResponseContextStore futureResponseContextStore,
                                    ApplicationEventPublisher applicationEventPublisher,
-                                   CentralSystemService16_SoapServer server) {
-        super(asyncTaskScheduler, ocppServerRepository, futureResponseContextStore, applicationEventPublisher);
+                                   CentralSystemService16_SoapServer server,
+                                   SteveConfiguration config) {
+        super(asyncTaskScheduler, ocppServerRepository, futureResponseContextStore, applicationEventPublisher, config);
         this.server = server;
         this.futureResponseContextStore = futureResponseContextStore;
     }
